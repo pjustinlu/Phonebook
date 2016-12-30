d5f37fb72f9367522b582a65d089862fc1db61d3
user_20 <user_20@mac-20deimac.local>
2016-12-30 18:41:16 +0800

    Initial Commit

---
+++ Demo/LoverDetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoverDetailViewController.swift
+//  Demo
+//
+//  Created by Peter Pan on 13/12/2016.
+//  Copyright © 2016 Peter Pan. All rights reserved.
+//
+
+import UIKit
+
+class LoverDetailViewController: UIViewController {
+
+    var loverInfoDic:[String:String]!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var interestLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //weightLabel.text = loverInfoDic["weight"]!
+        heightLabel.text = loverInfoDic["star"]!
+        //interestLabel.text = loverInfoDic["interest"]!
+        
+        self.navigationItem.title = loverInfoDic["name"]!
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
